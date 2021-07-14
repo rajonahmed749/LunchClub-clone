4f21e6ded9f8e878cc3366e8b7ceb404575527ae
rajonahmed749 <raazon749@gmail.com>
2021-07-15 03:13:11 +0600

    login page designed

--- src/Components/shared/Login/Login.js
+++ src/Components/shared/Login/Login.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import logo from "../../../images/logo.svg";
+import loginPhoto from "../../../images/login-photo.svg";
+import signUp from "../../../images/signWithGoogle.PNG";
 
 const Login = () => {
     return (
-        <div>
-            <h3>THis is login page</h3>
-        </div>
+        <section>
+            <div className="text-center mt-5">
+                <img src={logo} alt="logo"/>
+            </div>            
+            <h1 className="text-center mt-3">Log in</h1>
+            <div className="row justify-content-center text-center mt-4">
+                <div className="col-md-5">
+                    <img className="mb-4" src={loginPhoto} alt="logoPhoto"/> <br/>
+                    <img onClick={() => alert("clicked")} src={signUp}alt="signupPhoto"/>
+                    <form>
+                        <div className="mb-3">
+                            <input type="email" className="form-control" id="exampleInputEmail1" placeholder="E-mail" size="sm"/>
+                        </div>                 
+                        <button type="button" className="btn btn-primary">Send me a Login link</button>
+                    </form>
+                </div>
+            </div>
+        </section>
     );
 };
 
---
+++ src/Components/shared/Login/firebase.config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyArq9tZjai0OHgYOigXkFpc2Vu3zWt5Ub8",
+    authDomain: "lunch-clone.firebaseapp.com",
+    projectId: "lunch-clone",
+    storageBucket: "lunch-clone.appspot.com",
+    messagingSenderId: "763299389479",
+    appId: "1:763299389479:web:fcda77f063b0c7d67aee02"
+};
