6eb74756cd5a3f346ac5de239e75ee579520f432
rajonahmed749 <raazon749@gmail.com>
2021-07-18 23:36:25 +0600

    navbar user icon added

--- src/Components/shared/Menubar/Menubar.js
+++ src/Components/shared/Menubar/Menubar.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { Navbar, Nav ,NavDropdown} from "react-bootstrap";
+import { Navbar, Nav, Dropdown } from "react-bootstrap";
 import { Link } from 'react-router-dom';
 import { useHistory } from "react-router-dom";
 import { UserContext } from '../../../App';
@@ -10,7 +10,10 @@ const MenuBar = () => {
     const [loggedInUser, setLoggedInUser] = useContext(UserContext);
     console.log(loggedInUser)
 
-
+    const logOut = () => {
+        setLoggedInUser({})
+        history.push("/")
+    }
     return (
         <Navbar bg="light" expand="lg">
             <Navbar.Brand >
@@ -21,7 +24,19 @@ const MenuBar = () => {
                 <Nav className="ml-auto">
                     <Nav.Link>Meeting</Nav.Link>
                     <Nav.Link >Chat</Nav.Link>
-                    <Nav.Link >Invite</Nav.Link>                    
+                    <Nav.Link >Invite</Nav.Link>
+                    <Nav.Link >
+                        <Dropdown>
+                            <Dropdown.Toggle variant="link" id="dropdown">
+                                <img src={loggedInUser.img} alt="user-photo" className="userPhoto" />
+                            </Dropdown.Toggle>
+                            <Dropdown.Menu>
+                                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                                <Dropdown.Item href="#/action-2">Another</Dropdown.Item>
+                                <Dropdown.Item onClick={logOut}>Log out</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
