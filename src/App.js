6a1d1fdc0148b374eda9c93a0f9ea1220c0e6cb4
rajonahmed749 <raazon749@gmail.com>
2022-02-04 19:55:17 +0600

    ck editor added

--- src/App.js
+++ src/App.js
@@ -1,41 +1,26 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Home from './Components/Home/Home/Home';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
-import Weekly from './Components/Weekly/Weekly';
-import Schedule from './Components/Schedule/Schedule';
-import Login from './Components/shared/Login/Login';
-import { createContext, useState } from 'react';
-import Charity from './Components/Charity-page/Charity';
+import { useState } from 'react';
+import MyEditor from "./MyEditor";
 
-export const UserContext = createContext();
 
-function App() {
+function App(props) {
+  const [editor, setEditor] = useState(null);
+  console.log('from state', editor)
 
-  const [loggedInUser, setLoggedInUser] = useState({});
   return (
-    <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Charity/>
-          </Route>
-          <Route path="/weekly">
-            <Weekly />
-          </Route>
-          <Route path="/schedule">
-            <Schedule />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-        </Switch>
-      </Router>
-    </UserContext.Provider>
+    <>
+    <div>
+      <h2>This is ck editor code testing</h2>
+    </div>
+      <MyEditor
+        handleChange={(data) => {
+          setEditor(data);
+        }}
+        data={editor}
+        {...props}
+      />
+    </>
   );
 }
 
--- src/Components/shared/Login/firebase.config.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyArq9tZjai0OHgYOigXkFpc2Vu3zWt5Ub8",
-    authDomain: "lunch-clone.firebaseapp.com",
-    projectId: "lunch-clone",
-    storageBucket: "lunch-clone.appspot.com",
-    messagingSenderId: "763299389479",
-    appId: "1:763299389479:web:fcda77f063b0c7d67aee02"
-};
-export default firebaseConfig;
